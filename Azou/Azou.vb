41960405ff74861563f1501794d81bce365ada76
Cédric <cedced19@gmail.com>
2014-04-06 19:48:43 +0200

    4eme commit
    Beta 1.3

--- Azou/Azou.vb
+++ Azou/Azou.vb
@@ -22,7 +22,7 @@
 
     Sub SetLettre()
         Randomize()
-        Nombre1 = Int((42 * Rnd()) + 1)
+        Nombre1 = Int((78 * Rnd()) + 1)
 
         If Nombre1 = "1" Then Lettre = "a"
         If Nombre1 = "2" Then Lettre = "b"
@@ -66,6 +66,42 @@
         If Nombre1 = "40" Then Lettre = "é"
         If Nombre1 = "41" Then Lettre = "è"
         If Nombre1 = "42" Then Lettre = "@"
+        If Nombre1 = "43" Then Lettre = "a"
+        If Nombre1 = "44" Then Lettre = "b"
+        If Nombre1 = "45" Then Lettre = "c"
+        If Nombre1 = "46" Then Lettre = "d"
+        If Nombre1 = "47" Then Lettre = "e"
+        If Nombre1 = "48" Then Lettre = "f"
+        If Nombre1 = "49" Then Lettre = "g"
+        If Nombre1 = "50" Then Lettre = "h"
+        If Nombre1 = "51" Then Lettre = "i"
+        If Nombre1 = "52" Then Lettre = "j"
+        If Nombre1 = "53" Then Lettre = "k"
+        If Nombre1 = "54" Then Lettre = "l"
+        If Nombre1 = "55" Then Lettre = "m"
+        If Nombre1 = "56" Then Lettre = "n"
+        If Nombre1 = "57" Then Lettre = "o"
+        If Nombre1 = "58" Then Lettre = "p"
+        If Nombre1 = "59" Then Lettre = "q"
+        If Nombre1 = "60" Then Lettre = "r"
+        If Nombre1 = "61" Then Lettre = "s"
+        If Nombre1 = "62" Then Lettre = "t"
+        If Nombre1 = "63" Then Lettre = "u"
+        If Nombre1 = "64" Then Lettre = "v"
+        If Nombre1 = "65" Then Lettre = "w"
+        If Nombre1 = "66" Then Lettre = "x"
+        If Nombre1 = "67" Then Lettre = "y"
+        If Nombre1 = "68" Then Lettre = "z"
+        If Nombre1 = "69" Then Lettre = "1"
+        If Nombre1 = "70" Then Lettre = "2"
+        If Nombre1 = "71" Then Lettre = "3"
+        If Nombre1 = "72" Then Lettre = "0"
+        If Nombre1 = "73" Then Lettre = "4"
+        If Nombre1 = "74" Then Lettre = "5"
+        If Nombre1 = "75" Then Lettre = "6"
+        If Nombre1 = "76" Then Lettre = "7"
+        If Nombre1 = "77" Then Lettre = "8"
+        If Nombre1 = "78" Then Lettre = "9"
     End Sub
 
 
@@ -80,7 +116,7 @@
 
 
     Private Sub Timer1_Tick(sender As Object, e As EventArgs) Handles Timer1.Tick
-        Label1.Text = "Tapez " & Lettre
+        Label1.Text = "Tapez   " & Lettre
         Label2.Text = "Temps " & Timetake & "s"
         Label3.Text = "Score " & Score & "/60"
         If TextBox1.Text = Lettre Then
